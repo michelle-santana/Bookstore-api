80dedc434d2241250794e4d09421e459dee1f111
michelle-santana <michelle.raiadrogasil@gmail.com>
2021-12-27 19:49:58 -0300

    Criação das classes de domain

---
+++ src/main/java/com/michelle/bookstore/domain/Livro.java
@@ -0,0 +1,86 @@
+package com.michelle.bookstore.domain;
+
+import java.util.Objects;
+
+public class Livro {
+	
+	private Integer id;
+	private String title;
+	private String authorName;
+	private String text;
+	private Categoria categoria;
+
+	public Livro() {
+		super();
+	}
+
+	public Livro(Integer id, String title, String authorName, String text) {
+		this.id = id;
+		this.title = title;
+		this.authorName = authorName;
+		this.text = text;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthorName() {
+		return authorName;
+	}
+
+	public void setAuthorName(String authorName) {
+		this.authorName = authorName;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Livro other = (Livro) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	
+	
+	
+	
+
+}
